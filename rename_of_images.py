db3085ed7c845910188c0f444924f8b8ddeb7d6c
Ajoy Das <79185765+Ajoy114@users.noreply.github.com>
2024-02-15 11:58:45 +0600

    Update rename_of_images.py

--- rename_of_images.py
+++ rename_of_images.py
@@ -29,19 +29,3 @@ if __name__ == "__main__":
     directory = "/content/drive/MyDrive/Dataset/class1"  # Replace with the path to your image directory
     new_name_prefix = "cook_house_waste"  # Specify the new name prefix
     rename_images_in_directory(directory, new_name_prefix)
-
-import os
-
-def rename_images_in_directory(directory, new_name_prefix):
-    for count, filename in enumerate(os.listdir(directory)):
-        if filename.endswith(('.jpg', '.jpeg', '.png', '.bmp', '.heic','.HEIC')):  # Change the file extension as needed
-            new_name = f"{new_name_prefix}_{count + 1}.jpg"
-            old_path = os.path.join(directory, filename)
-            new_path = os.path.join(directory, new_name)
-            os.rename(old_path, new_path)
-            print(f"Renamed: {filename} -> {new_name}")
-
-if __name__ == "__main__":
-    directory = "/content/drive/MyDrive/Dataset/class2"  # Replace with the path to your image directory
-    new_name_prefix = "Elastic_waste"  # Specify the new name prefix
-    rename_images_in_directory(directory, new_name_prefix)
